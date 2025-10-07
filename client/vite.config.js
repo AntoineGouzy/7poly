9743c529daa626df50e7d00480324786a2e28d5b
nils <nils.bousquet@etu.inp-n7.fr>
2025-10-07 14:34:42 +0200

    vite config js  modify

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,19 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 8080, 
+    open: true,
+    host: true,
+    proxy: {
+      '^/api.*': {
+      target :'http://localhost:3000',
+      ws: true,
+      changeOrigin: true,
+      secure: true,
+    }
+  }
+  // close server object
+},
+// close config object
 })
